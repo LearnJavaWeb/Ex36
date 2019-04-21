539d9e56b20ccee40e78b834af5730d1448811b3
Thinh <thinhle24081995@gmail.com>
2019-04-22 00:22:39 +0700

    [Thực hành] Sử dụng Spring Security

---
+++ src/main/java/thinhluffy/com/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package thinhluffy.com;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
